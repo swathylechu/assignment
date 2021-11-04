c8801abf29a16848e9539e4b0a1a4abf033da90f
swathylechu <93258578+swathylechu@users.noreply.github.com>
2021-11-04 19:26:08 +0530

    new git
    assignment

---
+++ javascript foundation/foundation.js
@@ -0,0 +1,66 @@
+let a=1
+let b=3
+
+// ADDITION OPERATOR
+
+//let add= a + b;
+//console.log(add);
+
+//SUBTRACTION OPERATOR
+
+//let sub = a - b;
+//console. log(sub);
+
+//multiplication operator
+
+//let mult =a * b;
+//console.log(mult);
+
+//division operator
+
+let div= a/ b;
+console.log(div);
+
+//reminder operator
+
+let mod =a % b;
+console.log(mod);
+
+//prefix increment "there is a doubt in this"
+
+let c = ++a
+console.log(c);
+
+
+//postfix
+
+//let d = a++
+//console.log(d);
+
+// comparision operators
+
+//let x = 10;
+//let y = 5;
+
+//console.log(x == y);
+//console.log(x != y);
+//console.log(x > y);
+//console.log (x >= y);
+//console.log(x < y );
+//console.log (x<= y);
+
+//assignment operators
+
+let d = 10;
+console.log (d+=3);
+console.log (d -=4);
+console.log(d/=3);
+
+//LOGICAL OPERATORS
+
+let x = true;
+let y = false;
+
+console.log(x&&y);
+console.log(x||y);
+console.log(!y);
